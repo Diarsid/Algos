3cb6f916d17689fdb1f7ba3176e84938a8042beb
Diarsid <diarsid@gmail.com>
2016-02-25 23:41:55 +0200

    Binary search algorithm added.

---
+++ src/main/java/diarsid/algos/BinarySearch.java
@@ -0,0 +1,53 @@
+package diarsid.algos;
+
+/**
+ * @author Diarsid
+ */
+class BinarySearch {
+        
+    BinarySearch() {
+    }
+    
+    public static void main(String[] args) {
+        int[] data = {1, 5, 8, 12, 13, 18, 23, 56, 87, 95};
+        int asked = 1;
+        System.out.println("result: index of '" + asked + "' is: " + search(asked, data));
+    }
+    
+    static int search(final int asked, int [] data) {
+        if (data.length == 0) {
+            return -1;
+        }
+        
+        if (data.length == 1) {
+            if (data[0] == asked) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        
+        int from = 0;
+        int to = data.length;
+        int midIndex = getMiddleIndexBetween(from, to);
+        while ( from != to ) {    
+            if ( (midIndex == data.length) || (midIndex < 0) ) {
+                return -1;
+            }            
+            if ( asked == data[midIndex] ) {
+                return midIndex;
+            } else if ( asked > data[midIndex] ) {
+                from = midIndex + 1;
+                midIndex = getMiddleIndexBetween(from, to);
+            } else {
+                to = midIndex;
+                midIndex = getMiddleIndexBetween(from, to);
+            }
+        }
+        return -1;
+    }
+    
+    static int getMiddleIndexBetween(int from, int to) {
+        return (( (to - from) / 2 ) + from );
+    }
+}
---
+++ src/test/java/diarsid/algos/BinarySearchTest.java
@@ -0,0 +1,95 @@
+package diarsid.algos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static diarsid.algos.BinarySearch.search;
+
+/** *
+ * @author Diarsid
+ */
+public class BinarySearchTest {
+    
+    private int[] dataSet;
+            
+    @Test
+    public void testSearcExistedElementInEvenArray() {
+        dataSet = new int[]{2, 3, 6, 9, 12, 15, 17, 23, 34, 41};
+        int result = -1;
+        
+        result = search(9, dataSet);
+        assertEquals(3, result);
+        
+        result = search(34, dataSet);
+        assertEquals(8, result);
+        
+        result = search(2, dataSet);
+        assertEquals(0, result);
+        
+        result = search(41, dataSet);
+        assertEquals(9, result);
+    }
+    
+    @Test
+    public void testSearcExistedElementInOddArray() {
+        dataSet = new int[]{2, 3, 6, 9, 12, 15, 17, 23, 34};
+        int result = -1;
+        
+        result = search(9, dataSet);
+        assertEquals(3, result);
+        
+        result = search(17, dataSet);
+        assertEquals(6, result);
+        
+        result = search(2, dataSet);
+        assertEquals(0, result);
+        
+        result = search(34, dataSet);
+        assertEquals(8, result);
+    }
+    
+    @Test
+    public void searchUnexistedElement() {
+        dataSet = new int[]{2, 3, 6, 9, 12, 15, 17, 23, 34};
+        int result = 1;
+        
+        result = search(-28, dataSet);
+        assertEquals(-1, result);
+        
+        result = search(1000, dataSet);
+        assertEquals(-1, result);
+        
+        result = search(16, dataSet);
+        assertEquals(-1, result);
+        
+        result = search(5, dataSet);
+        assertEquals(-1, result);
+    }
+    
+    @Test
+    public void searchInEmptyArray() {
+        dataSet = new int[]{};
+        int result = 1;
+        
+        result = search(5, dataSet);
+        assertEquals(-1, result);
+    }
+    
+    @Test
+    public void searchExisetedInArrayWithOneElement() {
+        dataSet = new int[]{5};
+        int result = -1;
+        
+        result = search(5, dataSet);
+        assertEquals(0, result);
+    }
+    
+    @Test
+    public void searchUnexisetedInArrayWithOneElement() {
+        dataSet = new int[]{5};
+        int result = 0;
+        
+        result = search(8, dataSet);
+        assertEquals(-1, result);
+    }
+}
